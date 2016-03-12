ed9c65d2eb3b0599c90ef46de0dbae0857e33652
Tom Roeder <tmroeder@gmail.com>
2016-03-12 13:08:08 -0500

    Update the ES6 version of the state-machine comments.

--- port/state_machine.js
+++ port/state_machine.js
@@ -304,12 +304,11 @@ exports.states = {
   },
 
   // The second sound ends, realizing its projection.
-  // IAH: Finish transcribing.
   sound2Ends: {
     comment: "The second sound ends. Its duration is 'mensurally " +
       "determinate' because it has the potential for being precisely " +
       "reproduced. But it does not affect the projection of the first " +
-      "interonset duration, shown by the arrow and dashed arc",
+      "interonset duration, as shown.",
     message: "Click to begin the third sound.",
     transitions: {
       pause2: true,
@@ -324,32 +323,37 @@ exports.states = {
     }
   },
 
+  // The second sound ends without realizing its projection.
   sound2EndsWithoutProjection: {
     comment: "The second sound exceeds the duration projected at its onset.  " +
-      "The projection is not clearly realized, as indicated by the X " +
-      "through the dashed arc.",
-    message: "Click on the 'Back one step' button to select an earlier " +
-      "beginning for the second sound, or click 'Restart'.",
+      "The projection is not clearly realized, as indicated by the changed " +
+      "projection.",
+    message: "Restart to try again.",
     transitions: {
       start: true
     }
   },
 
+  // The second sound ends but is too long.
   sound2EndsTooLong: {
     comment: "The second sound is so long that it is mensurally " +
       "indeterminate.  Since the projected potential of the first " +
       "interonset duration is denied there is no projection at all.",
-    message: "Click on the 'Back one step' button to select an earlier " +
-      "beginning for the second sound, or click 'Restart'.",
+    message: "Restart to try again.",
     transitions: {
       start: true
     }
   },
 
+  //
+  // Pause 2
+  //
+  
+  // The second pause begins.
   pause2: {
     comment: "The silence between the second and third sounds is relatively " +
       "indeterminate if our attention is focused on the sounds' " +
-      "beginnings. The growing arc indicates that the duration from " +
+      "beginnings. The growing projection indicates that the duration from " +
       "the beginning of the second sound up to now, including the " +
       "silence, has 'projective potential' to be reproduced.",
     message: "Click to begin the third sound.",
@@ -391,10 +395,14 @@ exports.states = {
       if (points.isSlightlyLateNewProjection(first, x)) {
         return "sound3StartsSlightlyLateNewProjection";
       }
+
+      // This should not be possible to reach, but if it somehow is reached,
+      // then ignore the click and stay in pause2.
       return "pause2";
     }
   },
 
+  // The second pause is negative
   pause2Negative: {
     comment: "",
     message: "Click at the end of the second sound or later.",
@@ -410,11 +418,12 @@ exports.states = {
     }
   },
 
+  // The pause has lasted too long to be mensurally determinate.
   pause2TooLong: {
     comment: "The time since the beginning of the second sound is mensurally " +
       "indeterminate, having no projective potential to be reproduced.",
-    message: "Click button to begin the third sound (earlier if " +
-      "you want a projection).",
+    message: "Click to begin the third sound (earlier if you want a " +
+      "projection).",
     transitions: {
       pause2: true,
       pause2TooLong: true,
@@ -430,6 +439,11 @@ exports.states = {
     clickHandler: () => "sound3StartsTooLate"
   },
 
+  //
+  // Sound 3
+  //
+
+  // The third sound starts earlier than expected.
   sound3StartsAccel: {
     comment: "The beginning of the third sound is earlier than projected. " +
       "The second interonset duration is shorter than, but at least " +
@@ -437,8 +451,7 @@ exports.states = {
       "*acceleration* because we sense the realization of the first " +
       "projected duration even as we also perceive the difference " +
       "between the two durations.",
-    message: "Click on 'Back one step' to define a different third sound or " +
-      "'Restart' to begin again.",
+    message: "Restart to try again.",
     transitions: {
       pause2: true,
       start: true
@@ -452,14 +465,14 @@ exports.states = {
       "conditioned by the first, in which the second interonset " +
       "duration has the projective potential (the lower arrow) to be " +
       "reproduced.",
-    message: "Click on 'Back one step' to define a different third sound or " +
-      "'Restart' to begin again.",
+    message: "Restart to try again.",
     transitions: {
       pause2: true,
       start: true
     }
   },
 
+  // The third sound starts too late to be mensurally determinate.
   sound3StartsTooLate: {
     comment: "The projective potential of the first interonset duration (the " +
       "dashed arc) is realized, but the projective potential of the " +
@@ -470,20 +483,20 @@ exports.states = {
       "potential and a new beginning. A new and relatively " +
       "unconditioned potential emerges from the beginning of the " +
       "third sound.",
-    message: "Click on 'Back one step' to define a different third sound or " +
-      "'Restart' to begin again.",
+    message: "Restart to try again.",
     transitions: {
       pause2: true,
       start: true
     }
   },
 
+  // The third sound starts, realizing the first projection and suggesting a
+  // new projection.
   sound3StartsRealized: {
     comment: "The projection of the first interonset duration is realized. " +
-      "Another projection (the rightmost arrow and dashed arc) can be " +
-      "completed within the promised duration, so may enhance its " +
-      "mensural determinacy. The emergence of a new beginning, shown " +
-      "in parentheses, would clarify this.",
+      "As show, another projection can be completed within the promised " +
+      "duration, so may enhance its mensural determinacy. The emergence of " +
+      "a new beginning, shown in parentheses, would clarify this.",
     message: "Click anywhere to see an alternate interpretation.",
     transitions: {
       sound3StartsAltInterpretation: true
@@ -493,39 +506,38 @@ exports.states = {
   },
 
   sound3StartsAltInterpretation: {
-    comment: "In this interpretation the accent symbolizes an unequivocal " +
+    comment: "In this interpretation, the accent symbolizes an unequivocal " +
       "second beginning that denies the projection of the first " +
       "interonset duration in order to realize a larger projective " +
-      "potential, symbolized by the large arrow.",
-    message: "Click on 'Back one step' to define a different third sound or " +
-      "'Restart' to begin again.",
+      "potential.",
+    message: "Restart to try again",
     transitions: {
       pause2: true,
       start: true
     }
   },
 
+  // The third sound starts later than expected.
   sound3StartsSlightlyLate: {
     comment: "The beginning of the third sound is slightly later than " +
       "projected. We hear a *deceleration* because we sense the " +
       "realization of the first projected duration even as we also " +
       "perceive the difference between the two durations.",
-    message: "Click on 'Back one step' to define a different third sound or " +
-      "'Restart' to begin again.",
+    message: "Restart to try again.",
     transitions: {
       pause2: true,
       start: true
     }
   },
 
+  // The third sound starts slightly late and suggests a new projection.
   sound3StartsSlightlyLateNewProjection: {
     comment: "The third sound begins somewhat later than projected. A new " +
       "projection, indicated by the lowest arrow and dashed arc, " +
       "emerges, breaking off from the emerging first projection. We " +
       "reject the relevance of the first projection to the mensural " +
       "determinacy of the second interonset duration.",
-    message: "Click on 'Back one step' to define a different third sound or " +
-      "'Restart' to begin again.",
+    message: "Restart to try again."
     transitions: {
       pause2: true,
       start: true
@@ -534,7 +546,9 @@ exports.states = {
 };
 
 // writeGraph outputs a GraphViz diagram to check the state machine.
-// Generate the graph with the test/graph script.
+// Generate the graph with the test/graph script. The output of this function
+// must be wrapped in a graph statement like `strict digraph Meter{` (and with
+// a closing bracket at the end).
 exports.writeGraph = (states) => {
   let graph = "";
   for (let name in states) {
